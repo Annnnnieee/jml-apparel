c6d7b161de41251dcbbcf4a68bd11b0f62bd7adb
annnnnieee <anastasiya.vt.s@gmail.com>
2020-10-03 14:05:24 -0700

    formatted typography on product page

--- jml-apparel/src/Product/Product.js
+++ jml-apparel/src/Product/Product.js
@@ -12,8 +12,17 @@ import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
 
 const useStyles = makeStyles((theme) => ({
-    root:{
-        overflow: "hidden"
+    root: {
+        display: "flex",
+        overflow: "hidden",
+        fontFamily: "HelveticaNeue",
+        color: "#000000",
+        color: "rgb(0, 0, 0)",
+        flexDirection: "column",
+    },
+    introContainer: {
+        maxWidth: "90%",
+        margin: "auto",
     },
     itemContainer: {
         display: "flex",
@@ -24,21 +33,35 @@ const useStyles = makeStyles((theme) => ({
         display: "flex",
         flexDirection: "row",
         justifyContent: "space-between",
-        backgroundColor: "lightgrey",
-        padding: "50px 10px 30px 20px",
+        background : "rgba(194, 194, 194, 0.2)",
+        padding: "2% 3% 2% 3%",
         marginBottom: "10px",
         flexGrow: 1,
         flexShrink: 1,
-        },
-    itemImg: {
-        //paddingRight: "20px",
-        // flexShrink: 1,
-        // flexGrow: 1,
     },
-    itemDetails: {
-        flexShrink: 1,
-        flexGrow: 1,
-    }
+    itemImg: {
+        width: "83%",
+        height: "83%",
+    },
+    description: {
+        // fontSize: "3.375rem",
+        fontSize: "2.0vw",
+        maxWidth: "90%",
+
+    },
+    itemTitle: {
+        // fontSize: "3rem",
+        fontSize: "2.22vw",
+        textTransform: "uppercase",
+    },
+    itemList: {
+        // fontSize: "2.25rem",
+        fontSize: "1.6vw",
+        margin: "0",
+        listStylePosition: "inside",
+        padding: "0",
+    },
+
 }));
 
 function Product() {
@@ -94,56 +117,59 @@ function Product() {
     const classes = useStyles();
     return (
         <div className={classes.root}>
-            <img src={productTitle} alt="product title" />
-            <div>
-                <p>From labels and hangtags to elastics and tape, we supply hundreds of
-                new and innovative garment trims to enhance your fashion wear.
-                We work hard to be on top of the latest trends and to create the kind
-                of stylish, comfortable and practical clothing trims you demand
+            <div className={classes.introContainer}>
+                <img src={productTitle} alt="product cover image" />
+                <div className={classes.description}>
+                    <p>From labels and hangtags to elastics and tape, we supply hundreds of
+                    new and innovative garment trims to enhance your fashion wear.
+                    We work hard to be on top of the latest trends and to create the kind
+                    of stylish, comfortable and practical clothing trims you demand
                 </p>
 
-                <p>
-                    Count on us for forward-thinking apparel solutions to help you create
-                    distinctive quality garments.
+                    <p>
+                        Count on us for forward-thinking apparel solutions to help you create
+                        distinctive quality garments.
                 </p>
 
-                <p>
-                    We understand the importance of detail in your garment design process.
-                    At JML, we are committed to supporting you in meeting and exceeding your
-                    expectations. We take pride in our research and development, comprehensive
-                    knowledge of the industry and close relationsihp with our material suppliers
-                    as well as our reputation for efficiency and quick turn around.
+                    <p>
+                        We understand the importance of detail in your garment design process.
+                        At JML, we are committed to supporting you in meeting and exceeding your
+                        expectations. We take pride in our research and development, comprehensive
+                        knowledge of the industry and close relationship with our material suppliers
+                        as well as our reputation for efficiency and quick turn around.
                 </p>
 
-                <p>
-                    Our designers are constantly updating both the look and the construction
-                    of our clothing trims to match your own merchanidise. We adhere to the
-                    highest QC procedures and guarantee quality finished items.
+                    <p>
+                        Our designers are constantly updating both the look and the construction
+                        of our clothing trims to match your own merchanidise. We adhere to the
+                        highest QC procedures and guarantee quality finished items.
                 </p>
 
-                <ul>
-                    <li>Our factories are ISO 9001:2000 certified</li>
-                    <li>All our products meet Oeko-Tex Standard 100 test criteria</li>
-                    <li>Our labs are ITS accredited and can conduct tests according to ISO,
+                    <ul>
+                        <li>Our factories are ISO 9001:2000 certified</li>
+                        <li>All our products meet Oeko-Tex Standard 100 test criteria</li>
+                        <li>Our labs are ITS accredited and can conduct tests according to ISO,
                         AATCC and JIS standards</li>
-                </ul>
+                    </ul>
+                </div>
             </div>
 
             <div>
-
                 <Grid container spacing={1} className={classes.itemContainer} >
                     {
                         products.map(product => (
                             <Grid container xs={12} sm={6} spacing={1} className={classes.item} direction="row" >
-                                <Grid item xs={12} sm={6} spacing={1} className={classes.itemImg}> 
+                                <Grid item xs={12} sm={6} spacing={1} className={classes.itemImg}>
                                     <img src={product.imgSrc} />
                                 </Grid>
                                 <Grid item xs={12} sm={6} spacing={1}>
                                     <div>
-                                        <div>{product.description.title}</div>
-                                        {product.description.details.map(detail=> (
-                                            <li>{detail}</li>
-                                        ))}
+                                        <div className={classes.itemTitle}>{product.description.title}</div>
+                                        <ul className={classes.itemList}>
+                                            {product.description.details.map(detail => (
+                                                <li >{detail}</li>
+                                            ))}
+                                        </ul>
                                     </div>
                                 </Grid>
                             </Grid>
