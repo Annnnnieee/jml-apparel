433e2b7ca097fb4394f77a088c8a58c4c979ee27
annnnnieee <anastasiya.vt.s@gmail.com>
2020-10-12 22:55:09 -0700

    started about and styled awards

--- jml-apparel/src/About/About.js
+++ jml-apparel/src/About/About.js
@@ -1,11 +1,89 @@
 
 import React from 'react';
+import { Hidden } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
+import aboutCover from '../assets/about-title.jpg';
+import aboutCoverAlt from '../assets/about-alt-title.jpg';
+import about01 from '../assets/about-image01.jpg';
+import about02 from '../assets/about-image02.jpg';
+import about03 from '../assets/about-image03.jpg';
+import about04 from '../assets/about-image04.jpg';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: "flex",
+        overflow: "hidden",
+        flexDirection: "column",
+    },
+    introContainer: {
+        maxWidth: "90%",
+        margin: "auto",
+        marginBottom: "15px",
+    },
+    itemContainer: {
+        display: "flex",
+        justifyContent: "space-between",
+    },
+    item: {
+        background: "rgba(194, 194, 194, 0.2)",
+        padding: "3% 3% 3% 3%", // TODO fix this. 55 2 5
+        marginBottom: "10px",
+        flexGrow: 1,
+        flexShrink: 1,
+    },
+    images: {
+        marginTop: "2%",
+    },
+    itemImg: {
+        width: "83%",
+        height: "83%",
+        [theme.breakpoints.down('sm')]: {
+            width: "83%",
+            height: "auto",
+        },
+    },
+    // description: {
+    //     fontSize: "2.0vw",
+    //     maxWidth: "90%",
+    //     marginBottom: "3%",
+    //     [theme.breakpoints.down('sm')]: {
+    //         fontSize: '4vw',
+    //         maxWidth: "100%",
+    //         marginBottom: "7%",
+    //     },
+    // },
+    itemTitle: {
+        textTransform: "uppercase",
+        marginBottom: "10px",
+        [theme.breakpoints.down('sm')]: {
+            marginTop: "17px",
+        },
+    },
+    description: {
+        maxWidth: "90%",
+        [theme.breakpoints.down('sm')]: {
+            maxWidth: "100%"
+        },
+    },
+
+}));
 
 function About() {
+    const classes = useStyles();
     return (
         <div>
-            about us
-        </div>
+            <div className={classes.introContainer}>
+                <Hidden smDown>
+                    <img src={aboutCover} alt="about cover" />
+                </Hidden>
+                <Hidden mdUp>
+                    <img src={aboutCoverAlt} alt="about cover" />
+                </Hidden>
+            </div>
+            add two grids
+         </div>
     );
 }
 
--- jml-apparel/src/Home/Awards.js
+++ jml-apparel/src/Home/Awards.js
@@ -9,55 +9,57 @@ import Award03 from '../assets/lp-award03.jpg';
 import Award04 from '../assets/lp-award04.jpg';
 import Grid from '@material-ui/core/Grid';
 
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles(() => ({
+  root: {
+    maxWidth: "90%",
+    margin: "auto",
+    // background: "white",
+  },
+  title: {
+    textAlign: "center",
+  },
+  awardsGrid: {
+    marginTop: "1%",
+    marginBottom: "1%",
+  },
+  item: {
+    display: "block",
+    margin: "auto",
+    
+  },
+}));
+
+
+function Item(props) {
+  const classes = useStyles();
+
+  return (
+    <Grid item xs={12} sm={6} md={2} >
+      <img src={props.image} alt="award" className={classes.item} />
+    </Grid>
+  )
+
+}
+
 
 function Awards() {
-  return (
-    <div>
-        <Grid container className="award-section" spacing={1} justify="center">
-          <Grid item xs={12} sm={6} md={2} >
-            <Card >
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  image={Award01}
-                />
-              </CardActionArea>
-            </Card>
-          </Grid>
-          <Grid item xs={12} sm={6} md={2} >
-            <Card >
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  image={Award02}
-                />
-              </CardActionArea>
-            </Card>
-          </Grid>
-          <Grid item xs={12} sm={6} md={2}>
-            <Card className="product-card">
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  image={Award03}
-                />
-              </CardActionArea>
-            </Card>
-          </Grid>
-          <Grid item xs={12} sm={6} md={2} >
-            <Card >
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  image={Award04}
-                />
-              </CardActionArea>
-            </Card>
-          </Grid>
-        </Grid>
+  const classes = useStyles();
 
-      </div>
+  return (
+    <div className={classes.root}>
+      <Grid container className={classes.awardsGrid} spacing={1} justify="center">
+        <Item image={Award01}/>
+        <Item image={Award02}/>
+        <Item image={Award03}/>
+        <Item image={Award04}/>
+      </Grid>
+
+    </div>
   );
 }
+
 
 export default Awards;
