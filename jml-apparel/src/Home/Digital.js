36741a35cc8dc1843f99531d63f97dae57b06cdc
annnnnieee <anastasiya.vt.s@gmail.com>
2020-10-16 21:34:54 -0700

    added links to digital section

--- jml-apparel/src/Home/Digital.js
+++ jml-apparel/src/Home/Digital.js
@@ -3,15 +3,26 @@ import Grid from '@material-ui/core/Grid';
 import Digital01 from '../assets/lp-digital01.jpg';
 import Digital02 from '../assets/lp-digital02.jpg';
 import CustomCard from './CustomCard';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import { HashLinkWithScroll as HashLink } from '../HashLinkWithScroll/HashLinkWithScroll';
+
 
 function Digital() {
     return (
         <Grid container className="digital-section" spacing={1} justify="center">
             <Grid item xs={12} sm={6} key="digital01">
-                <CustomCard image={Digital01} title="3D Trims Library"></CustomCard>
+                <ButtonBase
+                    component={HashLink}
+                    to="/digital#3d-trims-library">
+                    <CustomCard image={Digital01} title="3D Trims Library" />
+                </ButtonBase>
             </Grid>
             <Grid item xs={12} sm={6} key="digital02">
-                <CustomCard image={Digital02} title="RFID Products"></CustomCard>
+                <ButtonBase
+                    component={HashLink}
+                    to="/digital#rfid-products">
+                    <CustomCard image={Digital02} title="RFID Products" />
+                </ButtonBase>
             </Grid>
         </Grid>
     );
